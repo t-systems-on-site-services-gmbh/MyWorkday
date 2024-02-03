a3c344395ae09233484c14936517964997fe5bf4
Andreas Weber <andreas.weber01@t-systems.com>
2024-02-03 11:06:47 +0100

    Projekt von https://start.spring.io/ heruntergeladen und Repository initial angelegt

---
+++ src/main/java/onsite/academy/myworkday/MyworkdayApplication.java
@@ -0,0 +1,13 @@
+package onsite.academy.myworkday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyworkdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyworkdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onsite/academy/myworkday/MyworkdayApplicationTests.java
@@ -0,0 +1,13 @@
+package onsite.academy.myworkday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyworkdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
